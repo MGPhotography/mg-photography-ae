561f0ae84527868fcb787f87b941214e23f5e4dc
gunalm4624 <gunaldesigns.official@gmail.com>
2024-12-31 08:12:50 +0530

    Sitemap Changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,24 +4,34 @@ export const dynamic = "force-static"; // Forces static generation
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mgphotographyae.com/#mg-home',
+      url: 'https://mgphotographyae.ae/#mg-home',
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://mgphotographyae.com/#about-us',
-      changeFrequency: 'weekly',
+      url: 'https://www.mgphotography.ae/india',
+      changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://mgphotographyae.com/#our-services',
+      url: 'https://mgphotographyae.ae/#about-us',
       changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
-      url: 'https://mgphotographyae.com//#contact-us',
+      url: 'https://mgphotographyae.ae/#our-services',
       changeFrequency: 'weekly',
       priority: 0.6,
+    },
+    {
+      url: 'https://mgphotography.ae/#contact-us',
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://mgphotography.ae/all-blogs',
+      changeFrequency: 'weekly',
+      priority: 0.5,
     }
   ]
 }
